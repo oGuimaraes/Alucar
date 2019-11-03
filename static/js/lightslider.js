3ab47a304dc12a2c4cabd641a25b50b119a91c56
oGuimaraes <otaviovgsr@gmail.com>
2019-11-02 21:46:41 -0300

    Carousel

--- static/js/lightslider.js
+++ static/js/lightslider.js
@@ -10,12 +10,12 @@
         useCSS: true,
         cssEasing: 'ease', //'cubic-bezier(0.25, 0, 0.25, 1)',
         easing: 'linear', //'for jquery animation',//
-        speed: 400, //ms'
+        speed: 1000, //ms'
         auto: true,
         pauseOnHover: false,
         loop: true,
         slideEndAnimation: true,
-        pause: 2000,
+        pause: 5000,
         keyPress: false,
         controls: true,
         prevHtml: '',
